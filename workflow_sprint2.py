635cb874f4ea67b77b56e60fe7c39f6b7ac777f9
dso1222 <dayne.okamura@daugherty.com>
2019-10-22 14:06:10 -0500

    changed query syntax to allow for apostrophe

--- workflow_sprint2.py
+++ workflow_sprint2.py
@@ -53,16 +53,16 @@ def dbentry():
     cursor = connection.cursor()
 
     #Inserting into Checklist Table
-    isOnboarding = request.json['isOnboarding']
+    isOnboarding = f"{request.json['isOnboarding']}"
     company = request.json['company']
     try:
         name = request.json['name']
     except: name = "Un-named"
 
 
-    checklist_query = f"INSERT INTO checklist (isonboarding, company, checklist_name)\
-        VALUES ('{isOnboarding}', '{company}', '{name}') RETURNING checklist_id"
-    cursor.execute(checklist_query)
+    checklist_query = f'INSERT INTO checklist (isonboarding, company, checklist_name)\
+        VALUES (%s, %s, %s) RETURNING checklist_id'
+    cursor.execute(checklist_query, (isOnboarding, company, name))
     checklist_id = cursor.fetchone()[0]
     connection.commit()
 
@@ -74,9 +74,9 @@ def dbentry():
     first_name = name[0].capitalize()
     last_name = name_2[0].capitalize()
 
-    consultant_query = f"INSERT INTO consultant (first_name, last_name, email)\
-        VALUES ('{first_name}', '{last_name}', '{email}') RETURNING consultant_id"
-    cursor.execute(consultant_query)
+    consultant_query = f'INSERT INTO consultant (first_name, last_name, email)\
+        VALUES (%s, %s, %s) RETURNING consultant_id'
+    cursor.execute(consultant_query, (first_name, last_name, email))
     consultant_id = cursor.fetchone()[0]
     connection.commit()
 
@@ -87,11 +87,11 @@ def dbentry():
     for x in range(len(Task)):
         description = Task[x]['description']
         reminder = Task[x]['reminder']
-        taskquery = f"INSERT INTO task (description, reminder) VALUES ('{description}', '{reminder}') RETURNING task_id"
-        cursor.execute(taskquery)
+        taskquery = f'INSERT INTO task (description, reminder) VALUES (%s, %s) RETURNING task_id'
+        cursor.execute(taskquery, (description, reminder))
         task_id = cursor.fetchone()[0]
         tidList.append({"task_id":task_id, "description":description})
-        progressquery = f"INSERT INTO progress (checklist_id, task_id) VALUES ({checklist_id}, {task_id})"
+        progressquery = f'INSERT INTO progress (checklist_id, task_id) VALUES ({checklist_id}, {task_id})'
         checklist_taskQuery = f"INSERT INTO checklist_task_join (checklist_id, task_id, consultant_id, date_sent) VALUES ({checklist_id}, {task_id}, {consultant_id}, '{date}')"
         cursor.execute(checklist_taskQuery)
         cursor.execute(progressquery)
@@ -113,12 +113,18 @@ def dbentry():
 
     task_string = ""
     for index in range(len(tidList)):
-        task_string += f"<br>-{tidList[index]['description']} <a href='http://127.0.0.1:5000/progress/{checklist_id}/{tidList[index]['task_id']}'>Click Here to Mark As Complete</a>"
+        task_string += f"<li style= 'font-family:arial,helvetica,sans-serif;'>{tidList[index]['description']}     \
+            <a href='http://127.0.0.1:5000/progress/{checklist_id}/{tidList[index]['task_id']}'>Click Here to Mark As Complete</a></li>"
     print(task_string)
 
     try:
         msg = Message(f"{company} {transition} Checklist for {first_name} {last_name}", sender="donotreply.daughertytransitions@gmail.com", recipients=[f"{email}"])
-        msg.html = f"<h2 style='text-align: center;'>{company} {transition} Checklist for {first_name} {last_name}</h2>{task_string}"
+        msg.html = f"<h2 style='text-align: center; font-family:arial,helvetica,sans-serif;'>{company} {transition} Checklist for {first_name} {last_name}</h2>\
+            <p style='text-align: left; font-family:arial,helvetica,sans-serif;'>To smooth the tranisition between engagements your team manager has compiled \
+            a list of the following task that must be completed.&nbsp;</p>\
+            <p style='text-align: left; font-family:arial,helvetica,sans-serif;'>Please complete each task and click the link to mark the task as completed.</p><p>&nbsp;</p><ol>{task_string}</ol>\
+            <p style= 'text-align: left; font-family:arial,helvetica,sans-serif;'>&nbsp;Thank you,</p>\
+                <p><img style='float: left;' src='https://www.daugherty.com/wp-content/uploads/2016/06/daugherty_stacked.jpg' alt='' width='238' height='74' /></p>"
         mail.send(msg)
         return json.dumps({"Status Code":200})
     except:
@@ -131,8 +137,8 @@ def progressUpdate(checklist_id, task_id):
     connection = connectPG()
     cursor = connection.cursor()
     date = datetime.now()
-    update = f"UPDATE progress SET iscomplete = true WHERE checklist_id = {checklist_id} and task_id = {task_id}"
-    date = f"UPDATE progress SET date_complete = '{date}' where checklist_id = {checklist_id} and task_id = {task_id}"
+    update = f'UPDATE progress SET iscomplete = true WHERE checklist_id = {checklist_id} and task_id = {task_id}'
+    date = f'UPDATE progress SET date_complete = "{date}" where checklist_id = {checklist_id} and task_id = {task_id}'
     cursor.execute(update)
     cursor.execute(date)
     connection.commit()
@@ -184,13 +190,13 @@ def home():
 def details(checklist_id):
     connection = connectPG()
     cursor = connection.cursor()
-    detailquery = f"SELECT first_name, last_name, company, isonboarding, date_sent, description, reminder, iscomplete, date_complete\
+    detailquery = f'SELECT first_name, last_name, company, isonboarding, date_sent, description, reminder, iscomplete, date_complete\
         from checklist_task_join ct\
         join progress p on ct.checklist_id = p.checklist_id and ct.task_id = p.task_id\
         join task t on ct.task_id = t.task_id\
         join checklist c on c.checklist_id = ct.checklist_id\
         join consultant co on co.consultant_id = ct.consultant_id\
-        where p.checklist_id = {checklist_id}"
+        where p.checklist_id = {checklist_id}'
 
     cursor.execute(detailquery)
     records = cursor.fetchall()
@@ -233,9 +239,9 @@ def savetemplate():
     except: name = "Un-named"
 
 
-    checklist_query = f"INSERT INTO checklist_template (isonboarding, company, checklist_name)\
-        VALUES ('{isOnboarding}', '{company}', '{name}') RETURNING checklisttemplate_id"
-    cursor.execute(checklist_query)
+    checklist_query = f'INSERT INTO checklist_template (isonboarding, company, checklist_name)\
+        VALUES (%s, %s, %s) RETURNING checklisttemplate_id'
+    cursor.execute(checklist_query, (isOnboarding, company, name))
     checklisttemplate_id = cursor.fetchone()[0]
     connection.commit()
 
@@ -244,8 +250,8 @@ def savetemplate():
     for x in range(len(Task)):
         description = Task[x]['description']
         reminder = Task[x]['reminder']
-        taskquery = f"INSERT INTO task_template (description,reminder) VALUES ('{description}', '{reminder}') RETURNING tasktemplate_id"
-        cursor.execute(taskquery)
+        taskquery = f'INSERT INTO task_template (description,reminder) VALUES (%s, %s) RETURNING tasktemplate_id'
+        cursor.execute(taskquery, (description, reminder))
         tasktemplate_id = cursor.fetchone()[0]
         joinquery = f'INSERT INTO template_join (checklisttemplate_id, tasktemplate_id) VALUES ({checklisttemplate_id},{tasktemplate_id})'
         cursor.execute(joinquery)
@@ -260,10 +266,10 @@ def savetemplate():
 def alltempaltes():
     connection = connectPG()
     cursor = connection.cursor()
-    query = f"Select ct.checklisttemplate_id, checklist_name\
+    query = f'Select ct.checklisttemplate_id, checklist_name\
                 from checklist_template ct\
                 join template_join tj on ct.checklisttemplate_id = tj.checklisttemplate_id\
-                join task_template tt on tt.tasktemplate_id = tj.tasktemplate_id"
+                join task_template tt on tt.tasktemplate_id = tj.tasktemplate_id'
     cursor.execute(query)
     records = cursor.fetchall()
     colnames = ['checklisttemplate_id', 'name']
@@ -279,11 +285,11 @@ def alltempaltes():
 def gettemplate(checklisttemplate_id):
     connection = connectPG()
     cursor = connection.cursor()
-    query = f"select ct.checklisttemplate_id, ct.checklist_name, isonboarding, company, description, reminder\
+    query = f'select ct.checklisttemplate_id, ct.checklist_name, isonboarding, company, description, reminder\
         from checklist_template ct\
         join template_join on ct.checklisttemplate_id = template_join.checklisttemplate_id\
         join task_template tt on tt.tasktemplate_id = tj.tasktemplate_id\
-        where ct.checklisttemplate_id = {checklisttemplate_id}"
+        where ct.checklisttemplate_id = {checklisttemplate_id}'
     cursor.execute(query)
     records = cursor.fetchall()
     tasklist = []
